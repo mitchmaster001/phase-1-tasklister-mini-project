6645385b4c24427245b3ff82686f1acddaf89d1a
Charles Njoroge <charles.maina@student.moringaschool.com>
2023-03-27 11:15:01 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const taskdescription = document.querySelector("task-descripton");
+
+checkbox.addEventListener("click", taskdescriptionClick, false);
+
+function taskdescriptionClick(event) {
+
+  let warn = "preventDefault() won't let you check this.<br>";
+  document.getElementById("submittask").innerHTML += warn;
+  // event.preventDefault();
+
+}
